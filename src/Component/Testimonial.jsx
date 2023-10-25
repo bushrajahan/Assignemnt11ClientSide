9b85d829805e7b3bd94e8dae0b12584f34d1be4c
bushrajahan <132329761+bushrajahan@users.noreply.github.com>
2023-10-25 20:22:44 +0600

    finished

---
+++ src/Component/Testimonial.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Testimonial = () => {
+  return (
+    <div>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas quidem perferendis sequi odit minima, ipsa quas perspiciatis suscipit aut, rerum assumenda doloremque, impedit accusantium! Maxime illum beatae sapiente rerum facilis?</p>
+    </div>
+  );
+};
+
+export default Testimonial;
